1bfdb26a2b86fa1087be86770b02fde540f7db83
Joao-Vitor-2008 <joaovitornascimentolopes4@gmail.com>
2026-08-17 18:54:08 -0400

    feat: experimenting with matrix

--- matrix.c
+++ matrix.c
@@ -2,44 +2,56 @@
 #include <stdlib.h>
 #include <time.h>
 
-int* create_random_matrix(int m, int n){
-  srand(time(NULL));   //gera uma seed aleatoria
-  
-  int* mtrz = malloc(sizeof(int) * (m*n));   //Aloca um espaço em brando na memoria do tamanho da matriz
+int* create_random_matrix(int m, int n) {
+	int* mtrz = malloc(sizeof(int) * (m * n));
 
-  for(int i = 0; i < m; i++){
-    for(int j = 0; j < n; j++){
-      mtrz[(i*n) + j] = (rand() % 200) + (-99);    //aleatoriza a matriz
-    }
-  }
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			mtrz[(i * n) + j] = (rand() % 200) - 99;
+		}
+	}
 
-  return mtrz;
+	return mtrz;
 }
 
-void print_matrix(int* ptr, int m, int n){
-  for(int i = 0; i < m; i++){
-    printf("[");
-    for(int j = 0; j < n; j++){
-      printf(" %d ", ptr[(i*n) + j]);
-    }
-    printf("]\n");
-  }
+void print_matrix(int* ptr, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		printf("[");
+		for (int j = 0; j < n; j++) {
+			printf(" %3d ", ptr[(i * n) + j]);
+		}
+		printf("]\n");
+	}
 }
 
-int main(){
-  int m;
-  int n;
+int max(int* arr, int size) {
+	int max_val = arr[0];
+	for (int i = 1; i < size; i++) {
+		if (arr[i] > max_val) {
+			max_val = arr[i];
+		}
+	}
+	return max_val;
+}
 
-  printf("n? ");
-  scanf("%d", &m);
-  printf("n? ");
-  scanf("%d", &n);
+int main() {
+	srand(time(NULL));
 
-  int* rm_ptr = create_random_matrix(m, n);
+	int m, n;
 
-  print_matrix(rm_ptr, m, n);
-  
-  free(rm_ptr);
+	printf("Linhas (m)? ");
+	scanf("%d", &m);
+	printf("Colunas (n)? ");
+	scanf("%d", &n);
 
-  return 0;
+	int* rm_ptr = create_random_matrix(m, n);
+
+	print_matrix(rm_ptr, m, n);
+
+	int ma = max(rm_ptr, m * n);
+	printf("Maior elemento: %d\n", ma);
+
+	free(rm_ptr);
+
+	return 0;
 }
