e007fbc9fc6ef6be40edc8b4471995cf7a2604b0
João Vitor <joaovitornascimentolopes4@gmail.com>
2026-08-21 06:56:54 -0400

    feat: final version
    Added a function to verify if a char is a letter and got better variables names

--- qtd_letra.c
+++ qtd_letra.c
@@ -3,41 +3,48 @@
 #include <stdbool.h>
 
 char to_lowercase(char c){
-	if(c >= 'A' && c <= 'Z'){
-		return c+32;
-	} else {
-		return c;
-	}
+        if(c >= 'A' && c <= 'Z'){
+                return c+32;
+        } else {
+                return c;
+        }
 }
 
-bool ocorre(char* str, char element, int i){
-	for(int j=0; j < i;j++){
-		if(element == str[j]){
-			return true;
-		}
-	}
-	return false;
+int is_letter(char c){
+        if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z')){
+                return 1;
+        } else {return 0;}
+}
+
+int ocorre(char* str, char element, int i){
+        for(int j=0; j < i;j++){
+                if(element == str[j]){
+                        return 1;
+                }
+        }
+        return 0;
 }
 
 int qtd_letra(char* frase, char letra){
-	if(frase[0] == '\0'){
-		return 0;
-	}
-
-	int counter = 0;
-
-	if(to_lowercase(letra) == to_lowercase(frase[0])){
-		counter++;
-	}
-
-	return counter + qtd_letra(&frase[1], letra);
+        if(frase[0] == '\0'){
+                return 0;
+        }
+        int counter = 0;
+        if(to_lowercase(letra) == to_lowercase(frase[0])){
+                counter++;
+        }
+        return counter + qtd_letra(&frase[1], letra);
 }
 
 int main(){
-	char frase[] = "jJoOo939+++--=";	
-	for (int i=0; frase[i] != '\0'; i++){
-		if( ( ('A' <= frase[i] && frase[i] <= 'Z') || ('a' <= frase[i] && frase[i] <= 'z') ) && ( ocorre(frase, frase[i], i) == false && ocorre(frase, to_lowercase(frase[i]), i) == false ) ){
-			printf("%c -- %d\n", frase[i], qtd_letra(frase, frase[i]));
-		}
-	}
+        int k = 0;
+        char frase[] = "jJoOo939+++--=vsnskdhdbnakjshshdjdjbshwsjhdhdjeiendbdhjdjsbehsjs**JJJ";        
+        for (int i=0; frase[i] != '\0'; i++){
+                char l = frase[i];
+                if( is_letter(l) && (!ocorre(frase, l, i) && !ocorre(frase, to_lowercase(l), i)) ){
+                        printf("%c -- %d\n", l, qtd_letra(frase, l));
+                        k += qtd_letra(frase, l);
+                }
+        }
+        printf("Tota de letras é: %d", k);
 }
