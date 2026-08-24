458736cbd7de9f27a687763e0f5dadcd985cd5e2
Joao-Vitor-2008 <joaovitornascimentolopes4@gmail.com>
2026-08-24 08:12:16 -0400

    feat: heist

--- qtd_letra.c
+++ qtd_letra.c
@@ -1,57 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-char to_lowercase(char c){
-        if(c >= 'A' && c <= 'Z'){
-                return c+32;
-        } else {
-                return c;
-        }
-}
+struct char_counter {
+	char c[2];
+	int counter;
+	long teste;
+};
 
 char to_uppercase(char c){
-	if(c >= 'a' && c <= 'z'){
-                return c-32;
-        } else {
-                return c;
-        }
+	if(c >= 'a' && c <= 'z')return c-32;
+	else return c;
 }
 
-int is_letter(char c){
-        if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z')){
-                return 1;
-        } else {return 0;}
+bool occurs(char* str, char element){
+	if(str[0] == '\0' || element == 0) return false;
+	else if(str[0] == element) {
+		return true;
+	} else {
+		return occurs(&str[1], element);
+	}
 }
 
-int ocorre(char* str, char element, int i){
-        for(int j=0; j < i;j++){
-                if(element == str[j]){
-                        return 1;
-                }
-        }
-        return 0;
+bool is_letter(char c, char* str){
+        if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z')) return true;
+	else return occurs(str, c );
 }
 
-int qtd_letra(char* frase, char letra){
-        if(frase[0] == '\0'){
-                return 0;
-        }
-        int counter = 0;
-        if(to_lowercase(letra) == to_lowercase(frase[0])){
-                counter++;
-        }
-        return counter + qtd_letra(&frase[1], letra);
+int qtd_letra(char* str, char character){
+        if(str[0] == '\0') return 0;
+        if(to_uppercase(character) == to_uppercase(str[0])) return 1 + qtd_letra(&str[1], character);
+        return 0 + qtd_letra(&str[1], character);
+}
+
+int* func(char* especials_chars, char* sentence){
+
 }
 
 int main(){
         int letter_counter = 0;
-        char frase[] = "jJoOaAãÃoO"; 
-        for (int i=0; frase[i] != '\0'; i++){
-                char l = frase[i];
-                if( is_letter(l) && (!ocorre(frase, l, i) && !ocorre(frase, to_lowercase(l), i)) ){
-                        printf("%c -- %d\n", l, qtd_letra(frase, l));
-                        letter_counter += qtd_letra(frase, l);
-                }
-        }
-        printf("Tota de letras é: %d", k);
+        char frase[] = "jJoOaAãÃoO";
+	char especials[] = "áéíóúâêôàãõçüÁÉÍÓÚÂÊÔÀÃÕÇÜ";
+	int *res = malloc(sizeof(struct char_counter) * (52 + sizeof(especials)-1));
+	
+	char c[];
+	printf("%d", sizeof(c));	
+
+
+	printf("Total de letras é: %d", letter_counter);
 }
