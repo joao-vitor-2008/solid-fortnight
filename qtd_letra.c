002cd11f5d834326a312bf6d8d46a9721c0e6eb2
Joao-Vitor-2008 <joaovitornascimentolopes4@gmail.com>
2026-08-28 23:23:27 -0400

    feat: i don't know what i am doing here

--- qtd_letra.c
+++ qtd_letra.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 typedef struct {
-	char c[4];
+	char c[5];
 	int counter;
 } char_counter;
 
@@ -13,18 +13,21 @@ char to_uppercase(char c){
 	else return c;
 }
 
-bool occurs(char* str, char element){
-	if(str[0] == '\0' || element == 0) return false;
-	else if(str[0] == element) {
+bool occurs(char* str, char* element){
+	if(str[0] == '\0' || *element == 0) return false;
+	else if(str[0] == *element) {
 		return true;
 	} else {
 		return occurs(&str[1], element);
 	}
 }
 
-bool is_letter(char c, char* str){
-        if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z')) return true;
-	else return occurs(str, c );
+bool is_letter(char *c){
+    if (('A' <= *c && *c <= 'Z') || ('a' <= *c && *c <= 'z')) return true;
+}
+
+bool is_especial(char* c, char* str){
+    return occurs(str, c);
 }
 
 int qtd_letra(char* str, char character){
@@ -34,11 +37,16 @@ int qtd_letra(char* str, char character){
 }
 
 char_counter* func(char* especials_chars, char* sentence){
-    char_counter* str = malloc( (52 + strlen(especials_chars)/2) * sizeof(char_counter) );
+    char buffer[80];
+    int k = 0;
+    char_counter* str = calloc( 52 + strlen(especials_chars)/2, sizeof(char_counter) );
     
     for(int i = 0; sentence[i] != '\0'; i++){
-        if(is_letter(sentence[i], especials_chars)){
-            
+        if(is_letter(&sentence[i]) && !occurs(buffer, &sentence[i]) && !is_especial(&sentence[i], especials_chars) ){
+            str[k].c = sentence[i];
+            str[k].counter = qtd_letra(sentence, sentence[i]);
+
+            k++;
         }
     }
 
@@ -52,5 +60,5 @@ int main(){
 
     char_counter* ptr = func(especials, sentence);
 
-	printf("Total de letras é: %zu", sizeof(ptr));
+	printf("Total de letras é: %d", letter_counter);
 }
